96e8f09a131e08cb9639a63db34e000da8cb8849
larigens@gmail.com <larigens@gmail.com>
2022-11-27 14:24:11 -0500

    organized js file and changed var names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
-
-// Variable/Constant Declaration
+// Constant Declaration
 const nine = 9;
 const twelve = 12;
 const eighteen = 18;
 const div = "<div>";
 
-// Wrapped all code that interacts with the DOM to ensure that the code isn't run until
-// the browser has finished rendering all the elements in the html.
-$(document).ready(function () {
+// Will only run once the page DOM is ready for JavaScript code to execute. Shorthand for $(document).ready(function ())
+$(function () {
 
-  // Added code to display the current date in the header of the page.
+  // Added code to display the current date in the page header.
   $("#currentDay").text(moment().format('MMMM Do YYYY, hh:mm a'));
 
-  createPlanner();
+  // Calls all functions so they can be executed.
+  renderPlanner();
   checkTime();
-  displayPlanner();
+  renderStorage();
 
 });
 
-function createPlanner() {
+// Creates the planner elements and attaches them.
+function renderPlanner() {
+  // Loop to continue generating the elements for all working hours.
   for (hour = nine; hour < eighteen; hour++) {
     var timeBlockContainer = $(div);
     timeBlockContainer.attr('id', 'hour-' + `${hour}`);
     timeBlockContainer.addClass("row");
 
+    // Child elements of the time block container.
     var hourContainer = $(div);
     hourContainer.addClass("col-1 hour");
     hourContainer.text(checkAmPm(hour));
@@ -36,29 +38,33 @@ function createPlanner() {
     buttonEl.addClass("col-1");
     buttonEl.text("Save");
 
-    var iconEl = $("<i aria-hidden='true'></i>");
+    var iconEl = $("<i>");
     iconEl.addClass("fa fa-save");
     buttonEl.prepend(iconEl);
 
+    // Appends the child elements to their parent time block container.
     timeBlockContainer.append(hourContainer);
     timeBlockContainer.append(textareaEl);
     timeBlockContainer.append(buttonEl);
 
+    // Attaches the time block container to the parent planner container.
     $("#planner").append(timeBlockContainer);
   }
 
+  // Added the event listener to the entire row.
   $(".row").click(function (event) {
-    var target = $(event.target);
-    if (target.is("button")) {
+    var target = $(event.target); // Event.target was added to locate where the user clicked.
+    if (target.is("button")) {  // Conditional statement to ensure that the function will only be executed if the button is clicked.
       var hourId = $(this).attr("id");
       var plan = $(this).find(".plan").val();
-      // Save in local storage
+      // Save to local storage with the hour id to ensure that the text will be saved and displayed only for that specific time block.
       localStorage.setItem(hourId, plan);
     }
   }
   )
 }
 
+// Function to make the displayed time more understandable for users, since the 24-hour model is not common.
 function checkAmPm(hour) {
   if (hour > twelve) {
     hour -= twelve;
@@ -73,24 +79,28 @@ function checkAmPm(hour) {
   return hour;
 }
 
+// Function to make each row color-coded to indicate whether it is in the past, present, or future.
 function checkTime() {
+  // Code in the 24-hour time for easy comparison.
   var currentHour = moment().hour();
   $(".row").each(function () {
-    var timeBlock = $(this).attr("id");
-    var checkHour = parseInt(timeBlock.slice(5));
-    if (currentHour > checkHour) {
+    var hourId = $(this).attr("id");
+    // Slices the string to get only the hour and converts it to a number for comparison.
+    var timeBlockHour = parseInt(hourId.slice(5));
+    if (currentHour > timeBlockHour) {
       $(this).addClass('past')
     }
-    else if (currentHour == checkHour) {
+    else if (currentHour == timeBlockHour) {
       $(this).addClass('present')
     }
-    else if (currentHour < checkHour) {
+    else if (currentHour < timeBlockHour) {
       $(this).addClass('future')
     }
   });
 }
 
-function displayPlanner() {
+// Function to display the local storage whenever the user loads the page.
+function renderStorage() {
   $(".row").each(function () {
     var hourId = $(this).attr("id");
     var plan = localStorage.getItem(hourId);
@@ -100,33 +110,3 @@ function displayPlanner() {
   }
   )
 }
-
-
-// TODO: Add a listener for click events on the save buttonEl. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the buttonEl that was clicked? How might the id be
-// useful when saving the description in local storage?
-// ----------- enter an event / WHEN I click the save buttonEl for that time block
-// THEN the text for that event is saved in local storage
-
-
-
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textareEl elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-
-
-
-
-
